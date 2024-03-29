864ddd570884428d750c470208d730aa1b6fdaf0
Szar <aly.hennessy@gmail.com>
2024-03-29 15:07:52 -0400

    Cleaning up, updated docs

--- src/AutoDocumentationProvider.php
+++ src/AutoDocumentationProvider.php
@@ -18,48 +18,17 @@ class AutoDocumentationProvider extends ServiceProvider
         {
             include __DIR__.'/../vendor/autoload.php';
         }
-
-
-        // SeacoastBank\AutoDocumentation\Providers\AutoDocumentationProvider::class,
-
-       /* $this->app['apidocumentation'] = $this->app->share(function($app)
-        {
-            return new AutoDocumentation(
-                new phpDocumentor\Reflection()
-            );
-        });*/
-
         $this->app->router->group(['namespace' => 'SeacoastBank\AutoDocumentation\App\Http\Controllers'],
             function(){
                 require __DIR__.'/routes/web.php';
             }
         );
-        $this->loadViewsFrom(__DIR__.'/views', 'SeacoastBank\AutoDocumentation');
-        $this->publishes(
-            [
-                __DIR__.'/views' => base_path('resources/views/vendor/seacoastbank/autodocumentation'),
-            ]
-        );
-        $this->publishes([
-            __DIR__.'/config' => config_path('vendor/seacoastbank/autodocumentation'),
-        ]);
+        //$this->publishes([ __DIR__.'/config' => config_path('vendor/seacoastbank/autodocumentation') ]);
 
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Generate::class
             ]);
         }
-
-        //$kernel = $this->app['Illuminate\Contracts\Http\Kernel'];
-        //$kernel->pushMiddleware('Yk\LaravelPackageExample\App\Http\Middleware\MiddlewareExample');
-
-
-
-
-        /*if ($this->app->runningInConsole()) {
-            $this->commands([
-                Generate::class
-            ]);
-        }*/
     }
 }
